b464555a5f65c6256f12ecb920bd6a0b2b9e81d4
Lucas <lucasdescm@gmail.com>
2022-01-11 18:04:44 +0100

    Mise en place de langages et formation

--- script.js
+++ script.js
@@ -20,3 +20,36 @@ window.addEventListener('scroll', function() {
     strings: ['Bonjour à vous, je m\'appelle Lucas et j\'ai 25 ans. </br> Après une carrière m\'ayant permis de découvrir plusieurs milieux professionnel et exercer plusieurs metiers différents. J\'ai décider de prendre un virage a 180° afin de devenir développeur, ayant suivis une formation d\'une année pour apprendre le code, je me suis donc formé aux differents langages du front tels que CSS, Javascript et son Framework Vue afin de devenir le plus rapidement possible développeur. Plus qu\'un avenir, je me suis découvert une nouvelle passion, celle de coder'],
     typeSpeed: 20,
   });
+
+
+//COMPTEUR LIVE
+
+let compteur = 0;
+
+$(window).scroll(function(){
+
+  const top = $('.counter').offset().top - window.innerHeight;
+
+  if(compteur == 0 && $(window).scrollTop() > top){
+    $('.counter-value').each(function(){
+      let $this = $(this),
+        countTo = $this.attr('data-count');
+        $({
+          countNum: $this.text()
+        }).animate({
+          countNum : countTo
+        },
+        {
+          duration: 10000,
+          easing: 'swing',
+          step: function(){
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function(){
+            $this.text(this.countNum);
+          }
+        });
+    });
+    compteur = 1;
+  }
+});
